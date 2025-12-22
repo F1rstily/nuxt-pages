964cf6462272a85d119da30fe37b31ed92cf6e98
Jet-Green <grishadzyin@gmail.com>
2025-12-23 00:44:03 +0500

    a

--- app/components/MyBlock.vue
+++ app/components/MyBlock.vue
@@ -1,23 +1,22 @@
-<!-- <template>
-    <UCard class="p-6 my-4 bg-white dark:bg-slate-800">
-      <h2 class="text-2xl font-bold mb-2">{{ title }}</h2>
-      <p class="mb-4">{{ text }}</p>
-      <UButton color="primary" @click="onClick">Подробнее</UButton>
-    </UCard>
-  </template>
-  
-  <script setup lang="ts">
-  defineProps<{
-    title: string
-    text: string
-  }>()
-  
-  const onClick = () => {
-    alert('Сиськи!')
-  }
-  </script>
-  
-  <style scoped>
-  /* Можно добавить свои стили */
-  </style>
-   -->
+<template>
+  <UCard class="p-6 my-4 bg-white dark:bg-slate-800">
+    <h2 class="text-2xl font-bold mb-2">{{ title }}</h2>
+    <p class="mb-4">{{ text }}</p>
+    <UButton color="primary" @click="onClick">Подробнее</UButton>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  text: string
+}>()
+
+const onClick = () => {
+  alert('Сиськи!')
+}
+</script>
+
+<style scoped>
+/* Можно добавить свои стили */
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ["@/styles/main.css"],
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/icon'],
+  ui: {
+    fonts: false,
+    // icons: ['lucide']
+  },
+
+  icon: {
+    serverBundle: {
+      collections: ['lucide']
+    }
+  }
 })
